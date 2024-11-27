1bc408a24f739a271ee936b3fa60b7c2a101a945
H-miura-fms <mmyha1289hm@gmail.com>
2024-11-27 19:34:15 +0900

    feat: SharedPreferencesプロバイダーを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'providers/shared_preference_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final prefs = await SharedPreferences.getInstance();
+
   runApp(
-    const ProviderScope(
-      child: MainApp(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          prefs,
+        ),
+      ],
+      child: const MainApp(),
     ),
   );
 }
